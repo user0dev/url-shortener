e19adca556d98e0b313329a474372612c73553be
user0dev <user0dev@yandex.com>
2017-02-17 18:50:45 +0300

    restory old UrlStorage

--- src/Storage/UrlStorage.php
+++ src/Storage/UrlStorage.php
@@ -12,7 +12,6 @@ namespace User0dev\UrlShortener\Storage;
 class UrlStorage
 {
     protected $pdo;
-
     public function __construct(array $config)
     {
         $dsn = sprintf("%s:dbname=%s;host=%s", $config["type"], $config["dbname"], $config["host"]);
@@ -20,40 +19,17 @@ class UrlStorage
         $this->pdo = new \PDO($dsn, $config["user"], $config["password"], $options);
         $this->pdo->exec(Queries::CREATE_TABLE);
     }
-
-    public function addLongUrl($longUrl, $shortUrl = "")
+    public function genShortUrl($longUrl)
     {
-        try {
-            $this->pdo->beginTransaction();
-
-            if (!shortUrl) {
-                $stmt = $this->pdo->prepare(Queries::INSERT_URL);
-                $stmt->bindValue(":long_url", $longUrl);
-                $stmt->bindValue(":short_url", "null", \PDO::PARAM_NULL);
-                $stmt->execute();
-                $newId = $this->pdo->lastInsertId("id");
-                $stmt = $this->pdo->prepare(Queries::UPDATE_SHORT_URL);
-                $stmt->bindValue(":id", $newId, \PDO::PARAM_INT);
-                $shortUrl = ConvertIntSymb::intToSymb($newId);
-                $stmt->bindValue(":short_url", $shortUrl);
-                $stmt->execute();
-            } else {
-                
-            }
-
-            $this->pdo->commit();
-        } catch (\Exception $e) {
-            $shortUrl = null;
-            $this->pdo->rollBack();
-        }
-        return $shortUrl;
+        $stmt = $this->pdo->prepare(Queries::INSERT_URL);
+        $stmt->execute([$longUrl]);
+        $newId = $this->pdo->lastInsertId("id");
+        return ConvertIntSymb::intToSymb($newId);
     }
-
     public function getLongUrl($shortUrl)
     {
         $id = ConvertIntSymb::SymbToInt($shortUrl);
         $stmt = $this->pdo->prepare(Queries::GET_LONG_URL);
-
         $stmt->bindValue(1, $id, \PDO::PARAM_INT);
         $stmt->execute();
         $result = $stmt->fetchAll();

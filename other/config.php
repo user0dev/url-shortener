527d8c76961a91daeb9fe7eefac0509634407adf
user0dev <user0dev@yandex.com>
2017-02-18 16:57:23 +0300

    add htaccess and htaccess with deny access to folder

---
+++ other/config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 18.02.17
+ * Time: 16:19
+ */
+$message = "You can access to config.php";
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $message ?></title>
+</head>
+<body>
+    <h1><?= $message ?></h1>
+</body>
+</html>
